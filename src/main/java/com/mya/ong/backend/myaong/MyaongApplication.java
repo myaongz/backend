f30547910936d481196724275f1b8397b5cc1a71
Jihyuk Lee <lead@sver.dev>
2024-04-05 12:02:25 +0900

    :heavy_plus_sign: :: 의존성 추가 및 시작 커밋

---
+++ src/main/java/com/mya/ong/backend/myaong/MyaongApplication.java
@@ -0,0 +1,13 @@
+package com.mya.ong.backend.myaong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mya/ong/backend/myaong/MyaongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mya.ong.backend.myaong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
