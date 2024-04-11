ec41cf2f6e9080de2a09a9f28b2df77911c1ca91
Jihyuk Lee <lead@sver.dev>
2024-04-11 22:39:06 +0900

    :closed_lock_with_key: :: DB 연결 정보 추가

--- src/main/java/com/mya/ong/backend/myaong/controller/HelloController.java
+++ src/main/java/com/mya/ong/backend/myaong/controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.mya.ong.backend.myaong.controller;public class HelloController {
+package com.mya.ong.backend.myaong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@ResponseBody
+@RestController
+
+public class HelloController {
+    @GetMapping("/")
+    public String hello () {
+        return "Hello World!";
+    }
 }
